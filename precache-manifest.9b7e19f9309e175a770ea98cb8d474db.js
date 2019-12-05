b7f237d9d429cc481d34b91afd196cc960e9bbe1
Osorio Neto <osor1o@hotmail.com>
2019-12-05 00:15:22 -0300

    Updates

---
+++ precache-manifest.9b7e19f9309e175a770ea98cb8d474db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dba141492a0ca53f223a46f22df0c90",
+    "url": "/Stack-Simulator/index.html"
+  },
+  {
+    "revision": "c5e67ea02bfccd16b661",
+    "url": "/Stack-Simulator/static/css/main.ff22b994.chunk.css"
+  },
+  {
+    "revision": "d920cb6f9ddc224bd028",
+    "url": "/Stack-Simulator/static/js/2.55ce36c0.chunk.js"
+  },
+  {
+    "revision": "c5e67ea02bfccd16b661",
+    "url": "/Stack-Simulator/static/js/main.10a684ba.chunk.js"
+  },
+  {
+    "revision": "067076bcb490132c416c",
+    "url": "/Stack-Simulator/static/js/runtime-main.4548002f.js"
+  }
+]);
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Stack from './components/Stack'
-
-function App() {
-  return <Stack/>
-}
-
-export default App;
